99b1c0aef1141e93d3cfdbb5a3649a94abb37b1e
thibault <thibault.latrace@gmail.com>
2020-06-10 12:38:52 +0200

    Version_with_images_bug

--- app.js
+++ app.js
@@ -5,10 +5,28 @@
 // $('<div><p>Ok, let\'s go !</p></div>').prependTo($('#conteneur')).attr('id', 'blocAccueil')
 // $('#blocAccueil').css({'position':'absolute', 'display':'flex', 'justify-content':'center', 'align-items':'center', 'z-index':1});
 
-// $('blocAccueil').css({'position':'absolute', 'display':'flex'});
+var étatAccueil = true;
+
+function accueil() {
+  $('#blocAccueil').css('z-index', 1);
+  $('#conteneur').css('filter', 'blur(2px)');
+  $('#conteneur').css('opacity', '50%', );
+  $('#blocInfosAccueil button').hover(function() {
+    $(this).css('cursor', 'pointer');
+  });
+  $('#blocInfosAccueil button').click(function() {
+    $('#blocAccueil').remove();
+    $('#conteneur').css('transition', 'opacity 0.5s', );
+    $('#conteneur').css('opacity', '100%', );
+    $('#conteneur').css('filter', 'none');
+    étatAccueil = false;
+  })
+}
 
 function main() {
-
+  console.log('exécution de main()');
+  console.log(premiere_exécution_main);
+  console.log(premiere_exécution_standby);
   $('.blocImage').each(function(){
 
     var selecteurBlocImage = this;
@@ -42,6 +60,43 @@ function main() {
     //Interactivité des blocTexte au survol de la souris
     $(selecteurBlocImage).find('.blocTexte p').hover(
       function() {
+        //Augmentation de la taille du texte de saison lorsqu'il est survolé
+        $(this).css('font-size', '90px'); //Attention : le this est ici $(this1).find('.blocTexte p') avec this1 le blocImage 
+        $(this).css('cursor', 'pointer');
+      },
+      function() {
+        //Diminution de la taille du texte de saison lorsqu'il n'est pas survolé
+        $(this).css('font-size', '80px');
+      }
+    );
+
+    //Changement de l'image au click sur la zone du texte de saison
+    $(selecteurBlocImage).find('.blocTexte p').click(function() {
+      console.log('click');
+      var selecteurImage = $(selecteurBlocImage).find('img');
+      changementImage(selecteurImage);
+    });
+
+    // // Agrandissement du bloc lorsqu'un mouvement de souris est détecté sur ce bloc :
+    // // utile lorsque la souris est restée inactive dans un bloc et recommence à bouger
+    // // car hover détecte seulement quand la souris rentre dans le bloc
+    // if (premiere_exécution_main) {
+    //   $(selecteurBlocImage).mousemove(function() {
+    //     agrandissementBloc();
+    //   });
+    // }
+  });
+}
+
+function animationBlocTexte() {
+
+  $('.blocImage').each(function(){
+
+    var selecteurBlocImage = this
+
+    //Interactivité des blocTexte au survol de la souris
+    $(selecteurBlocImage).find('.blocTexte p').hover(
+      function() {
         //Augmentation la taille du texte de saison lorsqu'il est survolé
         $(this).css('font-size', '90px'); //Attention : le this est ici $(this1).find('.blocTexte p') avec this1 le blocImage 
         $(this).css('cursor', 'pointer');
@@ -57,18 +112,10 @@ function main() {
       var selecteurImage = $(selecteurBlocImage).find('img');
       changementImage(selecteurImage);
     });
-
-    // Agrandissement du bloc lorsqu'un mouvement de souris est détecté sur ce bloc :
-    // utile lorsque la souris est restée inactive dans un bloc et recommence à bouger
-    // car hover détecte seulement quand la souris rentre dans le bloc
-    if (enStandby) {
-      $(selecteurBlocImage).mousemove(function() {
-        agrandissementBloc();
-        enStandby = false;
-      });
-    }
   });
 }
+
+
 
 //Permet de changer l'image de la saison correspondante
 function changementImage(selecteurImage){
@@ -101,18 +148,34 @@ var duréeInactivité = 0;
 var activité = false;
 var enStandby = false;
 
+//Si on n'a pas encore exécuté la fonction main(), premiere_exécution_main vaut false, et on lui attribue la valeur true après la première
+//éxécution, en réinitialisant la variable première_exécution_standby à false
+var premiere_exécution_main = true; 
+var premiere_exécution_standby = false;
+
 function détectionInactivité() {
     if (activité) {
-        duréeInactivité = 0;
-        activité = false; //remet à zéro l'activité toutes les "intervalle" secondes
-        main();
+      duréeInactivité = 0;
+      activité = false; //remet à zéro l'activité toutes les "intervalle" secondes
+      if (!premiere_exécution_main) {
+        main(); //important : l'exécuter avant de redonner les valeurs aux 2 variables ci-dessous car premiere_exécution_main
+                //est utilisée dans main()
+        premiere_exécution_main = true;
+        premiere_exécution_standby = false;
+      }
     }
     else {
-        duréeInactivité += intervalle;
-        if (duréeInactivité >= 3000) {
+      duréeInactivité += intervalle;
+      if (duréeInactivité >= 3000) {
+        enStandby = true;
+        if (!étatAccueil) { //ne permet pas l'exécution de standBy quand on est à l'accueil
+          if (!premiere_exécution_standby) {
             standby();
-            enStandby = true;
+            premiere_exécution_standby = true;
+            premiere_exécution_main = false;
+          }
         }
+      }
     }
     setTimeout(détectionInactivité, intervalle);
 };
@@ -121,9 +184,16 @@ function standby() {
   $('.blocImage').css('width', '25%');
   $('.blocTexte p').css('font-size', '60px');
   $('.blocTexteDates').hide();
+  console.log('exécution de standBy');
+  console.log(premiere_exécution_main);
+  console.log(premiere_exécution_standby);
 }
 
+accueil()
+
 main();
+
+// animationBlocTexte();
 
 détectionInactivité();
 
@@ -131,4 +201,13 @@ $(document).mousemove(function() {
   activité = true;
 });
 
-// A faire : dans le main, la ligne 46 n'est pas exécutée apparemment à cause de la ligne 20 de agrandissement Bloc
+// A faire : problème car main() s'exécute chaque fois que la souris bouge mais on veut qu'elle s'exécute une seule fois au début de l'activité
+//de la souris --> agir au niveau de la fonction détectionInactivité
+
+//Pour le moment, on a créé une fonction animationBlocTexte, qui fait que le site est plus 'responsive' quand on clique sur les saisons
+//(les images changent beaucoup plus rapidement) --> sûrement dû à ce qui est cité ci dessus
+//Quand on aura réglé le problème ci-dessus, 'remettre' la fonction animationBlocTexte à sa place dans la fonction main()
+
+//problème : tous les 3 standby, la fonctionnalité changement d'image au click ne fonctionne pas
+//source problème : on exécute main() qui se rajoute sur le premier main() exécuté --> un click
+//creuser du côté de clearTimeOut et setTimeOut
